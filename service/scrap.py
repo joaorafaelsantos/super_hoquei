17d8a5feae41ad7d08478a971e3e183ad98a3b67
João Santos <joaorafaelsantos@outlook.pt>
2022-04-05 13:09:54 +0100

    Add python script to get games data

---
+++ service/scrap.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+import json
+
+# Get HTML from FPP website
+response = requests.get("http://hp.fpp.pt/agenda1.php?epoca=2021")
+html = response.text
+
+# Convert games to structured format
+soup = BeautifulSoup(html, "html.parser")
+competitions = soup.select("#desktop > h4")
+games = []
+for i, competition in enumerate(competitions):
+
+    game_rows = soup.select(f"#desktop .toca:nth-of-type({i+1}) > table > tr")
+
+    for game_row in game_rows:
+        game = {
+            "date": game_row.select_one(".coluna-data > p").get_text(separator=" ")
+            if game_row.select_one(".coluna-data > p")
+            else None,
+            "home_team": game_row.select_one(".visitada").get_text(separator=" ")
+            if game_row.select_one(".visitada")
+            else None,
+            "away_team": game_row.select_one(".visitante").get_text(separator=" ")
+            if game_row.select_one(".visitante")
+            else None,
+            "url": game_row.select_one(".coluna-opcoes > a")["href"]
+            if game_row.select_one(".coluna-opcoes > a")
+            else None,
+            "competition": competition.get_text().strip(),
+        }
+        games.append(game)
+
+# DB connection
+conn = sqlite3.connect("sh.db")
+c = conn.cursor()
+
+# Delete current games
+c.execute("DELETE FROM Game")
+# Insert new games
+c.executemany(
+    "INSERT INTO Game (date, home_team, away_team, url, competition) VALUES (:date, :home_team, :away_team, :url, :competition);",
+    games,
+)
+conn.commit()
+
+# Fetch games
+data = {}
+
+# Today games
+c.execute("SELECT * FROM Game WHERE date(date) = date('now')")
+rows = c.fetchall()
+today = []
+
+# extract column names
+column_names = [d[0] for d in c.description]
+
+for row in rows:
+    info = dict(zip(column_names, row))
+    today.append(info)
+
+data["today"] = today
+
+# Tomorrow games
+c.execute("SELECT * FROM Game WHERE date(date) = date('now', '+1 day')")
+rows = c.fetchall()
+tomorrow = []
+
+for row in rows:
+    info = dict(zip(column_names, row))
+    tomorrow.append(info)
+
+data["tomorrow"] = tomorrow
+
+# Generate JSON
+with open("../app/src/data.json", "w") as f:
+    json.dump(data, f)
